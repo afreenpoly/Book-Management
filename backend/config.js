3c87f68a26874104d778cd3f9dec145bb4484ff1
Afreen Poly <85273664+afreenpoly@users.noreply.github.com>
2024-02-04 23:36:06 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT=7000;
-export const uri =  "mongodb+srv://afreenpoly:afreenpoly123@studetails.ebwix9o.mongodb.net/?retryWrites=true&w=majority";
+export const uri =  "mongodb+srv://afreenpoly:pass@studetails.ebwix9o.mongodb.net/?retryWrites=true&w=majority";
